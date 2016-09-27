f14fe86f829b248e70dd866cba113e97e083d7be
m.dantonio@cineca.it <m.dantonio@cineca.it>
2016-09-27 12:47:50 +0200

    MultipleInput and MultipleAutocomplete fields

--- js/angular/common.config.js
+++ js/angular/common.config.js
@@ -22,8 +22,50 @@ function formlyConfig(formlyConfigProvider) {
               ' typeahead-show-hint=true'+
               ' class="form-control"'+
               '>',
-    wrapper: ['bootstrapLabel', 'bootstrapHasError'],
+    wrapper: ['bootstrapLabel', 'bootstrapHasError']
 
   });
+
+formlyConfigProvider.setType({
+    name: 'multiInput',
+    extends: 'input',
+    template: '<div class="input-group" ng-repeat="item in model[options.key] track by $index">'+
+              '    <input type="text" class="form-control" ng-model="model[options.key][$index]"></input>'+
+              '    <a ng-if="$index == 0" class="input-group-addon" uib-tooltip="Cannot remove the first element">x</a>'+
+              '    <a ng-if="$index > 0" class="input-group-addon" uib-tooltip="Remove this element" ng-click="model[options.key].splice($index, 1)">x</a>'+
+              '</div>'+
+              '<div class="text-right">'+
+              '    <button type="button" class="btn btn-success btn-sm" ng-click="model[options.key].push('+"''"+')">Add new element</button>'+
+              '</div>',
+    wrapper: ['bootstrapLabel', 'bootstrapHasError']
+
+  });
+
+
+formlyConfigProvider.setType({
+    name: 'multiAutocomplete',
+    extends: 'input',
+    template: '<div class="input-group" ng-repeat="item in model[options.key] track by $index">'+
+              // '    <input type="text" class="form-control" ng-model="model[options.key][$index]" id="model[options.key]" name="model[options.key]"></input>'+
+
+              '<input type="text" '+
+              ' ng-model="model[options.key][$index]" '+
+              ' ng-model-options="{ getterSetter: true }"'+
+              ' uib-typeahead="item as item.name for item in ctrl.querySearch(options.key, $viewValue)"'+
+              ' typeahead-select-on-blur=true'+
+              ' typeahead-show-hint=true'+
+              ' class="form-control"'+
+              '>'+
+              '    <a ng-if="model[options.key].length == 1" class="input-group-addon" uib-tooltip="Cannot remove the first element">x</a>'+
+              '    <a ng-if="model[options.key].length > 1" class="input-group-addon" uib-tooltip="Remove this element" ng-click="model[options.key].splice($index, 1)">x</a>'+
+              '</div>'+
+              '<div class="text-right">'+
+              '    <button type="button" class="btn btn-success btn-sm" ng-click="model[options.key].push('+"''"+')">Add new element</button>'+
+              '</div>',
+    wrapper: ['bootstrapLabel', 'bootstrapHasError']
+
+  });
+
+
 
 }
--- js/angular/common.formly.service.js
+++ js/angular/common.formly.service.js
@@ -3,6 +3,10 @@
 
 angular.module('web').service('FormlyService', FormlyService);
 
+function isArray(obj){
+    return !!obj && Array === obj.constructor;
+}
+
 function FormlyService(noty)
 {
 
@@ -16,64 +20,77 @@ function FormlyService(noty)
 			var s = schema[i];
 			var k = s.key;
 
-			var ftype = "";
-			var ttype = "";
+			var field_type = "";
+			var template_type = "";
 
 			var field = {}
+			var multiple = ('multiple' in s && s['multiple'] == "true")
 			field['templateOptions'] = {}
 
-
 			if (s['type'] == "text") {
-				ftype = "input";
-				ttype = "text";
+				field_type = "input";
+				template_type = "text";
+				if (multiple) {
+					field['templateOptions']["inputOptions"] = {}
+					field['templateOptions']["inputOptions"]["type"] = field_type;
+					field['type'] = "multiInput"
+				}
 			} else if (s['type'] == "longtext") {
-				ftype = "textarea";
-				ttype = "text";
+				field_type = "textarea";
+				template_type = "text";
 			} else if (s['type'] == "int") {
-				ftype = "input";
-				ttype = "number";
+				field_type = "input";
+				template_type = "number";
 			} else if (s['type'] == "date") {
-				ftype = "input";
-				ttype = "date";
+				field_type = "input";
+				template_type = "date";
 			} else if (s['type'] == "select") {
-				ftype = "select";
-				ttype = "select";
+				field_type = "select";
+				template_type = "select";
 			} else if (s['type'] == "autocomplete") {
 				// Custom defined type
-				ftype = "autocomplete";
-				ttype = "autocomplete";
-			} else if (s['type'] == "typeahead") {
-				// Custom defined type (bootstrap) - to ovveride autocomplete
-				ftype = "typeahead";
-				ttype = "typeahead";
+				field_type = "autocomplete";
+				template_type = "autocomplete";
+
+				if (multiple) {
+					field['templateOptions']["inputOptions"] = {}
+					field['templateOptions']["inputOptions"]["type"] = field_type;
+					field_type = "multiAutocomplete"
+				}
+
 			}
 
 			field['key'] = s['key'];
-			field['type'] = ftype; 
+			field['type'] = field_type ; 
 			if ('default' in s)
 				field['defaultValue'] = s['default'];
 
 			field['templateOptions']['label'] = s['label'];
 			field['templateOptions']['placeholder'] = s['description'];
-			field['templateOptions']['type'] = ttype; 
+			field['templateOptions']['type'] = template_type; 
 			field['templateOptions']['required'] = (s['required'] == "true");
-			if (ttype == 'textarea') {
+			if (template_type == 'textarea') {
 				field['templateOptions']['rows'] = 5;
 			}
-			if (ttype == 'select') {
+			if (template_type == 'select') {
 				field['templateOptions']['labelProp'] = "value";
       			field['templateOptions']['valueProp'] = "id";
       			field['templateOptions']['options'] = s['options']
       			//field['templateOptions']['multiple'] = false;
 			}
 
-			if (ttype == 'autocomplete') {
+			if (template_type == 'autocomplete') {
 				field['controller'] = DataController+" as ctrl";
 			}
-			if (ttype == 'date') {
-				console.log(data)
-			}
+			// if (template_type == 'date') {
+			// 	console.log(data)
+			// }
 
+			if (multiple) {
+				if (!isArray(model[k])) {
+					model[k] = [model[k]];
+				}
+			}
 
 			fields.push(field);
 
@@ -85,7 +102,7 @@ function FormlyService(noty)
 				} else {
 					model[k] = data[k];
 				}
-				if (ttype == "number") {
+				if (template_type == "number") {
 					model[k] = parseInt(model[k]);
 				}
 			}
