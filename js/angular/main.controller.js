a838b8aaf6872b678d32fe9cea6a488fa53b7600
m.dantonio@cineca.it <m.dantonio@cineca.it>
2016-09-30 07:15:13 +0200

    Cleaning

--- js/angular/main.controller.js
+++ js/angular/main.controller.js
@@ -1,46 +1,17 @@
 (function() {
   'use strict';
 
-angular.module('web')
-    .controller('MainController', MainController);
+angular.module('web').controller('MainController', MainController);
 
-function MainController($scope, $rootScope, $log,
-    $timeout, $auth ,$state, api
-    //, hotkeys, keyshortcuts
-    )
+function MainController($scope, $rootScope, $log, $timeout, $state, api)
 {
     // Init controller
     var self = this;
     $log.debug("This is the main controller");
 
-// GLOBAL LOGGED
-// USE A WATCH ON SOMETHING
-    //$rootScope.logged = false;
-/*
-    // In case i am already logged, skip
-    if ($auth.isAuthenticated())
-    {
-        $rootScope.logged = true;
-    }
-*/
-
     // Passing a global variable
     self.templateDir = templateDir;
-    // self.customTemplateDir = customTemplateDir;
     self.blueprintTemplateDir = blueprintTemplateDir;
-
-    var color = 'cyan darken-3';
-    self.originalColor = angular.copy(color);
-    self.toolbarColor = angular.copy(color);
-    self.shade = "md-whiteframe-8dp";
-    //self.shade = 'z-depth-2';
-
-/*
-    // Utilities
-    $rootScope.goTo = function (element) {
-        $state.go(element.state.name, element.params);
-    };
-*/
 
     // Init the models
     $rootScope.loaders = {};
@@ -157,42 +128,6 @@ function MainController($scope, $rootScope, $log,
         //$log.debug("History stacks", temporaryRoutingHistory, totalRoutingHistory);
       }
     )
-
-/*
-    ////////////////////////////
-    // Control states to create the menu
-    $rootScope.menu = [];
-    var myObj = $state.get();
-
-    // DO THE MENU
-	forEach(myObj, function (x, i) {
-        //$log.debug("Menu element", i , x);
-
-        if (x.name.slice(loggedKey.length).indexOf('.') < 0
-            && x.url.indexOf(':') < 0)
-        {
-        // Skip if:
-            // a - the view is a sub child
-            // b - there is a parameter into the url (so direct link won't work)
-
-    		if (checkLoggedState(x)) {
-                var name = x.name.substr(loggedKey.length);
-                if (name != 'specialsearch') {
-                    $rootScope.menu.push(name.capitalizeFirstLetter());
-                }
-    		}
-        }
-	});
-
-    // In case of special search available
-    // Redirect to that state
-    $rootScope.activateSearch = function ()
-    {
-        $state.go('logged.specialsearch');
-    }
-
-	$log.info("Menu", $rootScope.menu);
-*/
 
 }
 
