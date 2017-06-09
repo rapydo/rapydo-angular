e13133a782a942a3653c6857d5abd3d2c4714703
Mattia D'Antonio <m.dantonio@cineca.it>
2017-06-09 11:32:46 +0200

    Removed unused code

--- commons/__init__.py
+++ commons/__init__.py
@@ -13,17 +13,6 @@ except BaseException:
     __version__ = 'unknown'
 
 #######################
-# Look for the best chance for json lib
-
-# import json as original_json
-# try:
-#     import simplejson as json
-#     # import commentjson as json
-# except:
-#     import json
-# JSON_EXT = 'json'
-
-#######################
 # Make a json and yaml test
 json.dumps({})
 
@@ -65,57 +54,9 @@ PATH = 'specs'
 # NOTE: this decides about final configuration
 DEBUG = True
 
-########################################
-
-# backend_linked = 'BACKEND_1_PORT' in os.environ
-
-# if backend_linked:
-#     # DIRECT ACCESS TO BACKEND
-#     BACKEND_PUBLIC_PORT = 8081
-# else:
-#     # ACCESS VIA PROXY
-#     # NOTE: IF SSL is ENABLED this port will be changed to 443 in pages.py
-#     BACKEND_PUBLIC_PORT = 80
-
-# # TO BE USED TO REDIRECT AUTHENTICATION FROM FRONTEND TO BACKEND
-# BACKEND_PRIVATE_PORT = \
-#     os.environ.get('BACKEND_1_PORT', "8080").split(':').pop()
-
-#################################
-# THE APP
-
 # DEBUG = os.environ.get('API_DEBUG', default_debug)
 DEBUG = os.environ.get('API_DEBUG', None)
 
 PRODUCTION = False
 if os.environ.get('APP_MODE', '') == 'production':
     PRODUCTION = True
-
-
-########################################
-# def get_api_url():
-#     """ Get api URL and PORT
-
-#     Usefull to handle https and similar
-#     unfiltering what is changed from nginx and container network configuration
-
-#     Warning: it works only if called inside a Flask endpoint
-#     """
-
-#     import re
-#     from flask import request
-#     from urllib.parse import urlparse
-
-#     backend_port = BACKEND_PUBLIC_PORT
-#     api_url = request.url_root
-
-#     if PRODUCTION:
-#         parsed = urlparse(api_url)
-#         if parsed.port is not None and parsed.port == 443:
-#             backend_port = parsed.port
-#             removed_port = re.sub(r':[\d]+$', '', parsed.netloc)
-#             api_url = parsed._replace(
-#                 scheme="https", netloc=removed_port
-#             ).geturl()
-
-#     return api_url, backend_port
