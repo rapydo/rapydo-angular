120eb42baf0357bcf6a6c2fd0b211fb2e4e59409
Mattia D'Antonio <m.dantonio@cineca.it>
2017-11-24 14:40:57 +0100

    Bug fix

--- js/angular/common.formly.service.js
+++ js/angular/common.formly.service.js
@@ -298,7 +298,9 @@ function FormlyService(noty, $log)
 							default_data = default_data[s["select_id"]];
 							default_data = default_data.toString()
 						}
-						if ("description" in default_data && "key" in default_data) {
+
+						if (typeof default_data["key"] !== 'undefined' &&
+								typeof default_data["description"] !== 'undefined') {
 							default_data = default_data["key"];
 						}
 
