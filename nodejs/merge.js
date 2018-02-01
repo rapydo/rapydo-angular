9ef43764ed2d763451651857dd986df7aecbfa99
Mattia <m.dantonio81@gmail.com>
2018-02-01 11:21:24 +0100

    Merge script to combine commons and custom package.json files

--- nodejs/merge.js
+++ nodejs/merge.js
@@ -3,9 +3,11 @@
 const merge = require('package-merge');
 const fs = require('fs')
 
-var commons = fs.readFileSync('/rapydo/package.json');
+var commons = fs.readFileSync('/rapydo/ng2/package.json');
+
+var angularjs = fs.readFileSync('/rapydo/ng2/package.ng1.json');
 
 var custom = fs.readFileSync('/app/frontend/package.json');
 
-fs.writeFileSync("/modules/package.json", merge(commons, custom));
+fs.writeFileSync("/modules/package.json", merge(commons, angularjs, custom));
 
