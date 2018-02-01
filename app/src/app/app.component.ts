b9b8f26043a833cab44dab6b2e6ac1f2d0affc1f
Mattia <m.dantonio81@gmail.com>
2018-02-01 11:28:19 +0100

    Not working configuration with webpack and angular

---
+++ app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import '../assets/css/styles.css';
+
+@Component({
+  selector: 'app-root',
+  template: `<div>test!!</div>`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent { }
