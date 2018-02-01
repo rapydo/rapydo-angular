76216da9e269593fb58137720f55a670fa0b3ead
Mattia <m.dantonio81@gmail.com>
2018-02-01 11:27:09 +0100

    Not working configuration with systemjs and angular5, compiled at runtime

---
+++ notworking/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import '../assets/css/styles.css';
+
+@Component({
+  selector: 'app-root',
+  templateURL: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent { }
