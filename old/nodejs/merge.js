9cf4afb52637054312862f7e76c55882d0b807bd
Mattia <m.dantonio81@gmail.com>
2018-02-01 12:42:50 +0100

    Removed old stuffs, move previous 'ng2' to main folder

--- old/nodejs/merge.js
+++ old/nodejs/merge.js
@@ -3,9 +3,9 @@
 const merge = require('package-merge');
 const fs = require('fs')
 
-var commons = fs.readFileSync('/rapydo/ng2/package.json');
+var commons = fs.readFileSync('/rapydo/package.json');
 
-var angularjs = fs.readFileSync('/rapydo/ng2/package.ng1.json');
+var angularjs = fs.readFileSync('/rapydo/package.ng1.json');
 
 var custom = fs.readFileSync('/app/frontend/package.json');
 
