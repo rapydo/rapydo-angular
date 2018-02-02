afc75dbd79b8632b712c098685fd6e684d6c525e
Mattia <m.dantonio81@gmail.com>
2018-02-02 06:14:19 +0100

    Just some print

--- utility/merge.js
+++ utility/merge.js
@@ -7,9 +7,11 @@ var commons = fs.readFileSync('/rapydo/package.json');
 
 var custom = fs.readFileSync('/app/frontend/package.json');
 
+console.log("********************************************************************************")
 console.log("Merging package files...");
 
 fs.writeFileSync("/modules/package.json", merge(commons, custom));
 
 console.log("[/rapydo/package.json] + [/app/frontend/package.json] -> [/modules/package.json]");
+console.log("********************************************************************************")
 
