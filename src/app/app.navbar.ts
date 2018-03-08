9c6bb468aae13a0e08ba502e7cfff8959da9851f
Mattia <m.dantonio81@gmail.com>
2018-03-08 19:21:39 +0100

    Installed both bootstrap4 and bootstrap3, tests with b4 modals in navbar

--- src/app/app.navbar.ts
+++ src/app/app.navbar.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'navbar',
@@ -8,7 +9,24 @@ export class NavbarComponent {
 
 	@Input() user: any;
 
-	constructor() {
+	constructor(private modalService: NgbModal) { }
+
+	logout(content) {
+	    this.modalService.open(content).result.then((result) => {
+	      console.log("Closed with: ${result}");
+	    }, (reason) => {
+	    	console.log(`Dismissed ${this.getDismissReason(reason)}`);
+	    });
+	}
+
+	private getDismissReason(reason: any): string {
+	    if (reason === ModalDismissReasons.ESC) {
+	      return 'by pressing ESC';
+	    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+	      return 'by clicking on a backdrop';
+	    } else {
+	      return  `with: ${reason}`;
+	    }
 	}
 
 }
