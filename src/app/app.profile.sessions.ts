4dff75f6b9ec1dd764ec962f4a1b0c525bbcb978
Mattia <m.dantonio81@gmail.com>
2018-04-05 10:47:37 +0200

    Implemented token revoke from sessions page

--- src/app/app.profile.sessions.ts
+++ src/app/app.profile.sessions.ts
@@ -1,23 +1,62 @@
 
 import { Component } from '@angular/core';
+import { catchError, map } from 'rxjs/operators';
 
 import { ApiService } from './api.service';
 import { AuthService } from './app.auth.service';
+import { NotificationService} from './app.notification.service';
 
 @Component({
   selector: 'sessions',
-  providers: [ApiService, AuthService],
+  providers: [ApiService, AuthService, NotificationService],
   templateUrl: './app.profile.sessions.html'
 })
 export class SessionsComponent { 
 
-  private user: any
+	private confirmationTitle = "Confirmation required";
+	private confirmationMessage = `
+		<div class='card text-center'>
+		<div class='card-body'>
+		<h4 class='card-title'>Are you really sure you want to invalidate this token?</h4>
+		<p class='card-text'>This token will no longer available. This operation cannot be undone.</p>
+		</div>
+		</div>
+		`;
 
-	constructor(api: ApiService, auth: AuthService) {
+    private tokens: any
 
-			//console.log(api.get());
-      this.user = auth.getUser();
+	constructor(private api: ApiService, private auth: AuthService, private notify: NotificationService) {
 
+      // this.user = auth.getUser();
+      	this.loadTokens();
+
+
+	}
+
+	loadTokens() {
+		this.api.get('tokens', "", [], {"base": "auth"}).subscribe(
+      		response => {
+      			this.tokens = response.data
+      		}
+       );
+
+	}
+
+	revokeToken(id) {
+
+		this.api.delete('tokens', id, {"base": "auth"}).subscribe(
+			response => {
+				console.log("invalidated")
+				this.notify.showSuccess("Token successfully revoked");
+				this.loadTokens();
+			}, error => {
+				this.notify.extractErrors(error, this.notify.ERROR);
+			}
+		);
+	}
+
+	cancelRequest() {
+		/*console.log("Request cancelled");*/
 	}
 
 }
