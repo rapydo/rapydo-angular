9dbd8b02747a987a9e3eee70e103d298f73c1e11
Mattia <m.dantonio81@gmail.com>
2018-04-23 10:47:10 +0200

    import bootstrap js

--- src/vendor.ts
+++ src/vendor.ts
@@ -14,3 +14,4 @@ import 'rxjs';
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
