ade2ccf0afe85d8390b237b0814956cf69e6f6b3
Mattia <m.dantonio81@gmail.com>
2018-05-16 10:49:06 +0200

    Admin users table, operations to be implemented

--- src/app/components/admin_users/admin_users.ts
+++ src/app/components/admin_users/admin_users.ts
@@ -1,5 +1,5 @@
 
-import { Component, OnInit, Input, ViewChild, TemplateRef } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
 
 import { ApiService } from '/rapydo/src/app/services/api';
 import { NotificationService} from '/rapydo/src/app/services/notification';
@@ -9,55 +9,53 @@ import { NotificationService} from '/rapydo/src/app/services/notification';
   providers: [ApiService, NotificationService],
   templateUrl: './admin_users.html'
 })
-export class AdminUsersComponent implements OnInit { 
+export class AdminUsersComponent implements OnInit {
 
-	@Input() private group: any;
-	@ViewChild('dataSize') public dataSize: TemplateRef<any>;
-	@ViewChild('dataModification') public dataModification: TemplateRef<any>;
-	@ViewChild('dataStatus') public dataStatus: TemplateRef<any>;
+	@ViewChild('dataRoles') public dataRoles: TemplateRef<any>;
+	@ViewChild('dataGroup') public dataGroup: TemplateRef<any>;
 
+	private loading:boolean = false;
 	private data: Array<any> = [];
 	private rows: Array<any> = [];
 	private columns: Array<any> = []
 	private page:number = 1;
-	private itemsPerPage:number = 10;
+	private itemsPerPage:number = 20;
 	private numPages:number = 1;
 	private dataLength:number = 0;
 
-	constructor(
-		private api: ApiService,
-		private notify: NotificationService,
-	) {
+	constructor(private api: ApiService, private notify: NotificationService) {}
 
-
-	}
 	public ngOnInit(): void {
 
 		this.list();
 
 		this.columns = [
-	        {name: 'Filename', prop: "name"},
-	        {name: 'Type', prop: "type"},
-	        {name: 'Size', prop: "size", cellTemplate: this.dataSize},
-	        {name: 'Uploaded', prop: "modification", cellTemplate: this.dataModification},
-	        {name: 'Status', prop: "status", cellTemplate: this.dataStatus},
+	        {name: 'Email', prop: "email"},
+	        {name: 'Name', prop: "name"},
+	        {name: 'Surname', prop: "surname"},
+	        {name: 'Group', prop: "_belongs_to", cellTemplate: this.dataGroup},
+	        {name: 'Roles', prop: "_roles", cellTemplate: this.dataRoles},
 		];
 	}
 
 	list() {
 
-		this.api.get('stage', this.group.id).subscribe(
+		this.loading = true;
+		this.api.get('admin/users').subscribe(
       		response => {
 
 				this.data = this.api.parseResponse(response.data);
+				console.log(this.data);
 				this.dataLength = this.data.length;
 				this.numPages = Math.ceil(this.dataLength / this.itemsPerPage);
 				this.rows = this.changePage(this.data);
 				console.log(this.data);
 				this.notify.extractErrors(response, this.notify.WARNING);
+				this.loading = false;
 			}, error => {
 				console.log(error);
       			this.notify.extractErrors(error, this.notify.ERROR);
+      			this.loading = false;
       		}
   		);
 
@@ -73,4 +71,5 @@ export class AdminUsersComponent implements OnInit {
 		this.page = page;
 		this.rows = this.changePage(this.data);
 	}
+
 }
