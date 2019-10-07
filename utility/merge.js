0c0ac7741c2232630713f436456cd22a81c5a366
Mattia <m.dantonio81@gmail.com>
2019-10-07 10:46:42 +0200

    Changed mountpoints to be angular-cli compliant

--- utility/merge.js
+++ utility/merge.js
@@ -3,15 +3,15 @@
 const merge = require('package-merge');
 const fs = require('fs');
 
-var commons = fs.readFileSync('/app/rapydo/package.json');
+var commons = fs.readFileSync('/app/app/rapydo/package.json');
 
-var custom = fs.readFileSync('/app/custom/package.json');
+var custom = fs.readFileSync('/app/app/custom/package.json');
 
 console.log("********************************************************************************")
 console.log("Merging package files...");
 
 fs.writeFileSync("/app/package.json", merge(commons, custom));
 
-console.log("[/app/rapydo/package.json] + [/app/custom/package.json] -> [/app/package.json]");
+console.log("[/app/app/rapydo/package.json] + [/app/app/custom/package.json] -> [/app/package.json]");
 console.log("********************************************************************************")
 
