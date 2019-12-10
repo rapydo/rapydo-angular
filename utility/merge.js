de2082474b34d785e30dbc8b26521ba412322d12
Mattia <m.dantonio81@gmail.com>
2019-12-10 13:36:46 +0100

    Experiments to merge base and custom angular.json (not enabled yet)

--- utility/merge.js
+++ utility/merge.js
@@ -3,15 +3,40 @@
 const merge = require('package-merge');
 const fs = require('fs');
 
-var commons = fs.readFileSync('/app/app/rapydo/package.json');
+var commons_package = fs.readFileSync('/app/app/rapydo/package.json');
+if (fs.existsSync('/app/app/custom/package.json')) {
 
-var custom = fs.readFileSync('/app/app/custom/package.json');
+	var custom = fs.readFileSync('/app/app/custom/package.json');
 
-console.log("********************************************************************************")
-console.log("Merging package files...");
+	console.log("\nMerging package files...");
 
-fs.writeFileSync("/app/package.json", merge(commons, custom));
+	fs.writeFileSync("/app/package.json", merge(commons_package, custom));
 
-console.log("[/app/app/rapydo/package.json] + [/app/app/custom/package.json] -> [/app/package.json]");
-console.log("********************************************************************************")
+	console.log("[/app/app/rapydo/package.json] + [/app/app/custom/package.json] -> [/app/package.json]\n");
+} else {
+	console.log("\nCopying package file...");
+	fs.writeFileSync("/app/package.json", commons_package);
+	console.log("/app/app/rapydo/package.json -> [/app/package.json]\n");
+}
 
+var commons_angular = fs.readFileSync('/app/app/rapydo/angular.json');
+if (fs.existsSync('/app/app/custom/angular.json')) {
+
+	/*
+	var custom = fs.readFileSync('/app/app/custom/angular.json');
+
+	console.log("\nMerging angular files...");
+
+	fs.writeFileSync("/app/angular.json", merge(commons_angular, custom));
+
+	console.log("[/app/app/rapydo/angular.json] + [/app/app/custom/angular.json] -> [/app/angular.json]\n");
+	*/
+
+	console.log("\nAngular.json merge is not supported, copying file...");
+	fs.writeFileSync("/app/angular.json", commons_angular);
+	console.log("/app/app/rapydo/angular.json -> [/app/angular.json]\n");
+} else {
+	console.log("\nCopying angular file...");
+	fs.writeFileSync("/app/angular.json", commons_angular);
+	console.log("/app/app/rapydo/angular.json -> [/app/angular.json]\n");
+}
