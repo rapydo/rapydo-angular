5b463f96f0482db0af87fca50922196abfb68b98
Mattia <m.dantonio81@gmail.com>
2020-03-16 13:48:05 +0100

    Testing AdminUsersComponent

---
+++ src/app/components/admin/admin_users.spec.ts
@@ -0,0 +1,29 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppModule } from '@rapydo/app.module';
+import { AdminUsersComponent } from '@rapydo/components/admin/admin_users';
+import { AdminModule } from '@rapydo/components/admin/admin.module';
+
+describe('AdminUsersComponent', () => {
+  let component: AdminUsersComponent;
+  let fixture: ComponentFixture<AdminUsersComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [AppModule, AdminModule]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdminUsersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+});
+
