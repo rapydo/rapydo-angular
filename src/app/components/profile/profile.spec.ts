fa2de628f6f8b2309e328dfccbaaa0d1231b1f45
Mattia <m.dantonio81@gmail.com>
2020-03-18 12:46:59 +0100

    Testing profile component

---
+++ src/app/components/profile/profile.spec.ts
@@ -0,0 +1,33 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppModule } from '@rapydo/app.module';
+import { ProfileComponent } from '@rapydo/components/profile/profile';
+import { ProfileModule } from '@rapydo/components/profile/profile.module';
+
+describe('ProfileComponent', () => {
+  let component: ProfileComponent;
+  let fixture: ComponentFixture<ProfileComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [AppModule, ProfileModule]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProfileComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('form invalid when empty', () => {
+    expect(component.form.valid).toBeFalsy();
+  });
+
+});
+
