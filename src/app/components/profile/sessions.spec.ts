433b52cc3b2991bd16837b25df89caf510c2c957
Mattia <m.dantonio81@gmail.com>
2020-03-18 15:50:06 +0100

    Testing sessions component

---
+++ src/app/components/profile/sessions.spec.ts
@@ -0,0 +1,33 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppModule } from '@rapydo/app.module';
+import { SessionsComponent } from '@rapydo/components/profile/profile';
+import { ProfileModule } from '@rapydo/components/profile/profile.module';
+
+describe('SessionsComponent', () => {
+  let component: SessionsComponent;
+  let fixture: ComponentFixture<SessionsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [AppModule, ProfileModule]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SessionsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('form invalid when empty', () => {
+    expect(component.form.valid).toBeFalsy();
+  });
+
+});
+
