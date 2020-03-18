354c82d767e1ae40dcf5e25f33cc7e18fa7505a1
Mattia <m.dantonio81@gmail.com>
2020-03-18 10:00:45 +0100

    Testing change password component

---
+++ src/app/components/profile/changepassword.spec.ts
@@ -0,0 +1,33 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppModule } from '@rapydo/app.module';
+import { ChangePasswordComponent } from '@rapydo/components/profile/changepassword';
+import { ProfileModule } from '@rapydo/components/profile/profile.module';
+
+describe('ChangePasswordComponent', () => {
+  let component: ChangePasswordComponent;
+  let fixture: ComponentFixture<ChangePasswordComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [AppModule, ProfileModule]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ChangePasswordComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('form invalid when empty', () => {
+    expect(component.form.valid).toBeFalsy();
+  });
+
+});
+
