3cafd75ec206b0804081e839a6258226772ef49b
Mattia <m.dantonio81@gmail.com>
2020-03-18 16:16:01 +0100

    Testing register and reset components

---
+++ src/app/components/public/register.spec.ts
@@ -0,0 +1,29 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppModule } from '@rapydo/app.module';
+import { RegisterComponent } from '@rapydo/components/public/register';
+import { PublicModule } from '@rapydo/components/public/public.module';
+
+describe('RegisterComponent', () => {
+  let component: RegisterComponent;
+  let fixture: ComponentFixture<RegisterComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [AppModule, PublicModule]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RegisterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+});
+
