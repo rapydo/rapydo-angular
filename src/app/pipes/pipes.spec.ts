cb404dd97f3675c78717c5f6743a973b95c41653
Mattia <m.dantonio81@gmail.com>
2020-03-19 08:05:53 +0100

    Bug fix

--- src/app/pipes/pipes.spec.ts
+++ src/app/pipes/pipes.spec.ts
@@ -21,12 +21,12 @@ describe('Pipes', () => {
     expect(bytes_pipe.transform(0)).toBe("0");
     expect(bytes_pipe.transform(-1)).toBe("-");
     expect(bytes_pipe.transform("XYZ")).toBe("-");
-    expect(bytes_pipe.transform(10).toBe("10 bytes");
-    expect(bytes_pipe.transform(10240).toBe("10 kB");
-    expect(bytes_pipe.transform(10485760).toBe("10 MB");
-    expect(bytes_pipe.transform(10737418240‬).toBe("10 GB");
-    expect(bytes_pipe.transform(‭10995116277760‬).toBe("10 TB");
-    // expect(bytes_pipe.transform(1,125899906842624e+16‬).toBe("10 PB");
+    expect(bytes_pipe.transform(10)).toBe("10 bytes");
+    expect(bytes_pipe.transform(10240)).toBe("10 kB");
+    expect(bytes_pipe.transform(10485760)).toBe("10 MB");
+    expect(bytes_pipe.transform(10737418240‬)).toBe("10 GB");
+    expect(bytes_pipe.transform(‭10995116277760‬)).toBe("10 TB");
+    // expect(bytes_pipe.transform(1,125899906842624e+16‬)).toBe("10 PB");
 
     // expect(bytes_pipe.transform({'a': 1, 'b': 2})).toBe([{'key': 'a', value: 1}, {'key': 'b', value: 2}]);
 
