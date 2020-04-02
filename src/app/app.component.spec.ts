5f74f7e6992263054f4cd30cf5a92614c73d5d8f
Mattia <m.dantonio81@gmail.com>
2020-04-02 13:57:48 +0200

    Added tests on app component

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,26 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppModule } from '@rapydo/app.module';
+import { AppComponent } from '@rapydo/app.component';
+
+describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [AppModule]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('component initialization', () => {
+    expect(component).toBeDefined();
+  });
+
+});
+
