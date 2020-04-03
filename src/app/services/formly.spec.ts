c954cb649852877e412ee448242ae41c2831b48b
Mattia <m.dantonio81@gmail.com>
2020-04-03 05:53:24 +0200

    Added tests on json2Form from formly service

--- src/app/services/formly.spec.ts
+++ src/app/services/formly.spec.ts
@@ -4,6 +4,8 @@ import { FormlyService } from '@rapydo/services/formly';
 describe('FormlyService', () => {
   let injector: TestBed;
   let service: FormlyService;
+  let schema = getSchema();
+  let model = getModel();
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -14,9 +16,109 @@ describe('FormlyService', () => {
     service = injector.get(FormlyService);
   });
 
-  it('json2Form - empty input', () => {
-    expect(service.json2Form(undefined, undefined)).toEqual({"fields":[], "model": {}});
+  it('json2Form - empty schema', () => {
+    let form = service.json2Form(undefined, {});
+    expect(form).not.toBeUndefined();
+    expect(form).toContain("fields");
+    expect(form).toContain("model");
+    expect(form['fields']).toEqual([]);
+    expect(form['model']).toEqual({});
   });
+
+  it('json2Form - invalid schema', () => {
+    let invalid = [
+      {
+        "invalid": "schema",
+        "missing": "name key"
+      },
+    ];
+    expect(service.json2Form(invalid, {})).toBeNull();
+  });
+
+  it('json2Form - empty model', () => {
+    let form = service.json2Form(schema, {});
+    expect(form).not.toBeUndefined();
+    expect(form).toContain("fields");
+    expect(form).toContain("model");
+    expect(form['fields']).not.toEqual([]);
+    expect(form['model']).not.toEqual({});
+    expect(form['model']).toContain('text');
+    expect(form['model']['text']).toEqual('');
+  });
+
+  it('json2Form - with model', () => {
+    let form = service.json2Form(schema, model);
+    expect(form).not.toBeUndefined();
+    expect(form).toContain("fields");
+    expect(form).toContain("model");
+    expect(form['fields']).not.toEqual([]);
+    expect(form['model']).not.toEqual({});
+    expect(form['model']).toContain('text');
+    expect(form['model']['text']).toEqual('xyz');
+  });
+
+  getSchema() {
+    return [
+      {
+        "custom": {
+            "label": "Text"
+        },
+        "description": "Text",
+        "in": "formData",
+        "name": "text",
+        "required": true,
+        "type": "string"
+      },
+      {
+        "custom": {
+          "label": "List"
+        },
+        "description": "List",
+        "enum": [
+          {
+            "k1": "val1"
+          },
+          {
+            "k2": "val2"
+          },
+        ],
+        "in": "formData",
+        "name": "list",
+        "required": true,
+        "type": "string"
+      },
+      {
+        "custom": {
+            "label": "Checkbox"
+        },
+        "default": true,
+        "in": "formData",
+        "name": "checkbox",
+        "required": false,
+        "type": "boolean"
+      },
+      {
+        "custom": {
+            "label": "Number"
+        },
+        "default": 1,
+        "description": "number",
+        "in": "formData",
+        "name": "number",
+        "required": true,
+        "type": "number"
+      },
+    ];
+  }
+
+  getModel() {
+    return {
+      'text': 'xyz'
+      'list': 'k2',
+      'checkbox': true,
+      'number': '7',
+    }
+  }
 
 
 });
