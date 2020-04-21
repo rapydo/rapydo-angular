9017e3084a7a49b8bd0c2c4bf790497a5f414840
Mattia <m.dantonio81@gmail.com>
2020-04-21 08:00:25 +0200

    Add mock api to routing tests

--- src/app/rapydo.module.spec.ts
+++ src/app/rapydo.module.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed, getTestBed, ComponentFixture, fakeAsync, tick } from '@angular/core/testing';
 import { RouterTestingModule } from "@angular/router/testing";
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { Location } from "@angular/common";
 import { Router } from "@angular/router";
 
@@ -11,16 +12,23 @@ describe('Core routing', () => {
   let injector: TestBed;
   let router: Router;
   let location: Location;
+  let httpMock: HttpTestingController;
   let fixture: ComponentFixture<AppComponent>;
+
+  const mock401Response = {
+    status: 401,
+    statusText: 'UNAUTHORIZED'
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, HttpClientTestingModule],
     });
 
     injector = getTestBed();
     router = injector.inject(Router);
-    location = TestBed.get(Location); (3)
+    httpMock = injector.inject(HttpTestingController);
+    location = TestBed.inject(Location);
 
   	// Creating the root component with the router-outlet so
   	// the router has somewhere to insert components.
@@ -34,22 +42,55 @@ describe('Core routing', () => {
 	  expect(location.path()).toBe('/public/reset');
 	}));
 
+  it('navigate to in offline mode', fakeAsync(() => {
+    router.navigate(['/app/login']);
+    tick();
+    expect(location.path()).toBe('/offline');
+
+    router.navigate(['/app/profile']);
+    tick();
+    expect(location.path()).toBe('/offline');
+
+    router.navigate(['/app/admin/users']);
+    tick();
+    expect(location.path()).toBe('/offline');
+
+  }));
+
   it('navigate to /app/login', fakeAsync(() => {
     router.navigate(['/app/login']);
     tick();
     expect(location.path()).toBe('/app/login');
+
+    const profile_req = httpMock.expectOne(environment.authApiUrl + '/profile');
+    expect(profile_req.request.method).toEqual('GET');
+    profile_req.flush('', mock401Response);
+
+    httpMock.verify();
   }));
 
   it('navigate to /app/profile', fakeAsync(() => {
     router.navigate(['/app/profile']);
     tick();
     expect(location.path()).toBe('/app/login');
+
+    const profile_req = httpMock.expectOne(environment.authApiUrl + '/profile');
+    expect(profile_req.request.method).toEqual('GET');
+    profile_req.flush('', mock401Response);
+
+    httpMock.verify();
   }));
 
   it('navigate to /app/admin/users', fakeAsync(() => {
     router.navigate(['/app/admin/users']);
     tick();
     expect(location.path()).toBe('/app/login');
+
+    const profile_req = httpMock.expectOne(environment.authApiUrl + '/profile');
+    expect(profile_req.request.method).toEqual('GET');
+    profile_req.flush('', mock401Response);
+
+    httpMock.verify();
   }));
 
 });
