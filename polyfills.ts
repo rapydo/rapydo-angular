36264977d936b62862bff4c4dd1f172a6f64581a
Mattia <m.dantonio81@gmail.com>
2020-04-22 16:48:50 +0200

    Undo isArray polyfill

--- polyfills.ts
+++ polyfills.ts
@@ -119,10 +119,3 @@ if (typeof (window as any).global === 'undefined') {
 // Angular 9 introduced a global $localize() function that needs to be loaded.
 // Please add import '@angular/localize'; to your polyfills.ts file.
 import '@angular/localize/init';
-
-// https://developer.mozilla.org/it/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
-if (!Array.isArray) {
-  Array.isArray = function(arg: any[]): boolean {
-    return Object.prototype.toString.call(arg) === '[object Array]';
-  };
-}
