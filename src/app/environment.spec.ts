e45f0b4e93dc95f9fd5e30f6eb38af161d0d5a35
Mattia <m.dantonio81@gmail.com>
2020-05-08 17:14:44 +0200

    Added environment test

--- src/app/environment.spec.ts
+++ src/app/environment.spec.ts
@@ -5,6 +5,23 @@ describe('Environment', () => {
 	it('environment', () => {
 
       	expect(environment).not.toBeUndefined();
+
+		expect(environment.production).not.toBeUndefined();
+		expect(environment.apiUrl).not.toBeUndefined();
+		expect(environment.authApiUrl).not.toBeUndefined();
+		// expect(environment.version).not.toBeUndefined();
+		// expect(environment.rapydo_version).not.toBeUndefined();
+		expect(environment.projectTitle).not.toBeUndefined();
+		expect(environment.projectDescription).not.toBeUndefined();
+		expect(environment.enableFooter).not.toBeUndefined();
+		expect(environment.allowRegistration).not.toBeUndefined();
+		expect(environment.allowPasswordReset).not.toBeUndefined();
+		expect(environment.websocketsUrl).not.toBeUndefined();
+		expect(environment.WRAP_RESPONSE).not.toBeUndefined();
+		expect(environment.SENTRY_URL).not.toBeUndefined();
+		expect(environment.GA_TRACKING_CODE).not.toBeUndefined();
+		expect(environment.ALL).not.toBeUndefined();
+
 	});
 
 });
