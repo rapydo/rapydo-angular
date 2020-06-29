668628f322fdb653e040cdaf58972480512d61f1
Mattia <m.dantonio81@gmail.com>
2020-06-29 11:55:15 +0200

    Added tests on login page

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -1,26 +1,52 @@
 describe("Profile", () => {
   beforeEach(() => {
     cy.visit("app/profile");
-  });
 
-  // before(() => {
-  //   let credentials = {
-  //       username: Cypress.env('AUTH_DEFAULT_USERNAME'),
-  //       password: Cypress.env('AUTH_DEFAULT_PASSWORD')
-  //   }
-  //   cy.request('POST', Cypress.env('API_URL')+'/auth/login', credentials)
-  //       .its('body')
-  //       .as('token')
-  // });
+    cy.get('button:contains("Ok, got it")').click();
 
-  it("cy.location() - get window.location", () => {
     cy.location().should((location) => {
       expect(location.pathname).to.eq("/app/login");
     });
-  });
 
-  it("cy.url() - get the current URL", () => {
     cy.url().should("include", "/app/login");
     cy.url().should("include", "?returnUrl=%2Fapp%2Fprofile");
+  });
+
+  it("Login - click on submit button", () => {
+    cy.get("input[id=formly_2_input_password_1]").type(
+      Cypress.env("AUTH_DEFAULT_PASSWORD")
+    );
+    cy.get("input[id=formly_2_input_username_0]").type(
+      Cypress.env("AUTH_DEFAULT_USERNAME")
+    );
+    cy.get("button").contains("Login").click();
+  });
+
+  it("Login - enter on password field", () => {
+    cy.get("input[id=formly_2_input_username_0]").type(
+      Cypress.env("AUTH_DEFAULT_USERNAME")
+    );
+    cy.get("input[id=formly_2_input_password_1]").type(
+      Cypress.env("AUTH_DEFAULT_PASSWORD") + "{enter}"
+    );
+  });
+
+  it("Login - enter on username field", () => {
+    cy.get("input[id=formly_2_input_password_1]").type(
+      Cypress.env("AUTH_DEFAULT_PASSWORD")
+    );
+    cy.get("input[id=formly_2_input_username_0]").type(
+      Cypress.env("AUTH_DEFAULT_USERNAME") + "{enter}"
+    );
+  });
+
+  afterEach(() => {
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq("/app/profile");
+    });
+
+    cy.get("a").find(".fa-sign-out-alt").parent().click();
+
+    cy.get("button").contains("Confirm").click();
   });
 });
