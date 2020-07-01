dc6711f00aaf93d2d1b1e74ba28ca79910e50c14
Mattia <m.dantonio81@gmail.com>
2020-07-01 22:01:49 +0200

    Tests fix

--- cypress/integration/admin_users.spec.ts
+++ cypress/integration/admin_users.spec.ts
@@ -25,18 +25,18 @@ describe("AdminUsers", () => {
 
     cy.get('button:contains("new user")').click();
 
-    cy.get('button:contains("Submit")').click();
+    cy.get('button:contains("Submit")').click({ force: true });
     cy.get("formly-validation-message").contains("This field is required");
 
     cy.get("input[id=formly_11_input_email_0]").clear().type("invalid");
-    cy.get('button:contains("Submit")').click();
+    cy.get('button:contains("Submit")').click({ force: true });
     cy.get("formly-validation-message").contains("Invalid email address");
 
     cy.get("input[id=formly_11_input_email_0]")
       .clear()
       .type(Cypress.env("AUTH_DEFAULT_USERNAME"));
     cy.get("input[id=formly_11_input_password_1]").clear().type("short");
-    cy.get('button:contains("Submit")').click();
+    cy.get('button:contains("Submit")').click({ force: true });
     cy.get("formly-validation-message").contains(
       "Should have at least 8 characters"
     );
@@ -44,9 +44,7 @@ describe("AdminUsers", () => {
     cy.get("input[id=formly_11_input_password_1]").clear().type("looooong");
     cy.get("input[id=formly_11_input_name_2]").clear().type("SampleName");
     cy.get("input[id=formly_11_input_surname_3]").clear().type("SampleSurname");
-    cy.get('button:contains("Submit")').click();
-
-    // to be completed: should fail because DEFAULT_USERNAME already exists
+    cy.get('button:contains("Submit")').click({ force: true });
 
     cy.get("div[role=alertdialog]")
       .contains(
@@ -57,7 +55,7 @@ describe("AdminUsers", () => {
     cy.get("input[id=formly_11_input_email_0]")
       .clear()
       .type("new-user@sample.org");
-    cy.get('button:contains("Submit")').click();
+    cy.get('button:contains("Submit")').click({ force: true });
 
     cy.get("div[role=alertdialog]")
       .contains("Confirmation: user successfully created")
