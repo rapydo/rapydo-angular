f82dd15352ada9e1e51a1987dc07c3bde0c10c8d
Mattia <m.dantonio81@gmail.com>
2020-07-12 11:12:07 +0200

    # Test fix /3

--- cypress/integration/terms.of.use.acceptance.spec.ts
+++ cypress/integration/terms.of.use.acceptance.spec.ts
@@ -45,6 +45,7 @@ describe("Terms of use", () => {
   });
 
   beforeEach(() => {
+    const username = "bbb0000000000000@sample.org";
     cy.visit("/app/login");
 
     cy.get("input[placeholder='Your username (email)']").clear().type(username);
@@ -54,8 +55,41 @@ describe("Terms of use", () => {
   });
 
   if (Cypress.env("ALLOW_TERMS_OF_USE")) {
-    it("Terms of Use acceptance", () => {
+    it("Terms of Use - not accepted", () => {
       cy.get("div.modal-header h4.modal-title").contains("Terms of use");
+
+      cy.get("div.modal-footer h4").contains(
+        "Do you accept all our Terms of Use?"
+      );
+
+      cy.get("div.modal-footer button").first().contains("YES");
+      cy.get("div.modal-footer button").last().contains("NO").click();
+
+      cy.checkalert(
+        "We apologize but you are not allowed to login, as you have not accepted our Terms of Use"
+      );
+    });
+
+    it("Terms of Use - accepted", () => {
+      cy.get("div.modal-header h4.modal-title").contains("Terms of use");
+
+      cy.get("div.modal-footer h4").contains(
+        "Do you accept all our Terms of Use?"
+      );
+
+      cy.get("div.modal-footer button").last().contains("NO");
+
+      cy.get("div.modal-footer button").first().contains("YES").click();
+
+      cy.location().should((location) => {
+        expect(location.pathname).to.eq("/app/profile");
+      });
+    });
+
+    it("Terms of Use - already accepted", () => {
+      cy.location().should((location) => {
+        expect(location.pathname).to.eq("/app/profile");
+      });
     });
   } else {
     it("Terms of Use not enabled", () => {
