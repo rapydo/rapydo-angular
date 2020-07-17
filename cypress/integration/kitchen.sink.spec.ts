88110fbb8de8cbf2f0aa2c97ceeb866a15650982
Mattia <m.dantonio81@gmail.com>
2020-07-17 19:53:21 +0200

    # Tests on the kitchen sink [form part]

--- cypress/integration/kitchen.sink.spec.ts
+++ cypress/integration/kitchen.sink.spec.ts
@@ -10,7 +10,71 @@ describe("KitchenSink", () => {
     cy.location("pathname").then((pathname) => {
       if (pathname == "/app/sink") {
         // Kitchen Sink is enabled, add here all tests!
-        cy.contains("Lorem ipsum dolor sit amet, consectetur adipiscing elit");
+
+        cy.get("div.card-header h4").contains("Kitchen Sink");
+
+        // Normal formly forms
+        cy.get("ul.nav-tabs li.nav-item a.active").contains("ngx-formly");
+
+        cy.get("formly-wrapper-form-field");
+        cy.get('button:contains("Submit")').click({ force: true });
+        cy.get("formly-validation-message")
+          .eq(0)
+          .contains("This field is required");
+        cy.get("formly-validation-message")
+          .eq(1)
+          .contains("This field is required");
+
+        cy.get('input[placeholder="email"]').clear().type("Invalid");
+        cy.get("formly-validation-message")
+          .eq(0)
+          .contains("Invalid email address");
+
+        cy.get('input[placeholder="email"]').clear().type("user@sample.org");
+        cy.get('input[placeholder="password"]')
+          .clear()
+          .type("thisIsVeryS3cret!");
+        cy.get('button:contains("Submit")').click({ force: true });
+
+        cy.contains('"email": "user@sample.org"');
+        cy.contains('"password": "thisIsVeryS3cret!"');
+
+        // Horizontal formly forms
+        cy.get("ul.nav-tabs li.nav-item a")
+          .contains("horizontal forms")
+          .click();
+
+        cy.get("formly-horizontal-wrapper");
+        cy.get('button:contains("Submit")').click({ force: true });
+        cy.get("formly-validation-message")
+          .eq(0)
+          .contains("This field is required");
+        cy.get("formly-validation-message")
+          .eq(1)
+          .contains("This field is required");
+
+        cy.get('input[placeholder="email"]').clear().type("Invalid");
+        cy.get("formly-validation-message")
+          .eq(0)
+          .contains("Invalid email address");
+
+        cy.get('input[placeholder="email"]').clear().type("user2@sample.org");
+        cy.get('input[placeholder="password"]')
+          .clear()
+          .type("thisIsSUPERS3cret!");
+        cy.get('button:contains("Submit")').click({ force: true });
+
+        cy.contains('"email": "user2@sample.org"');
+        cy.contains('"password": "thisIsSUPERS3cret!"');
+
+        // Upload
+
+        cy.get("ul.nav-tabs li.nav-item a").contains("ngx-uploadx").click();
+
+        // Datatables
+        cy.get("ul.nav-tabs li.nav-item a").contains("ngx-datatable").click();
+
+        // What more??
       }
     });
   });
