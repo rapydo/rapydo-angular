8154849f697f7de1af8b78aae2e44935d39de18d
Mattia <m.dantonio81@gmail.com>
2020-08-03 08:45:08 +0200

    # Tests fix

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -66,3 +66,6 @@ Cypress.Commands.add("checkalert", (msg) => {
 Cypress.Commands.add("getmail", () => {
   return cy.readFile("/logs/mock.mail.lastsent.body");
 });
+Cypress.Commands.add("parseXlsx", (inputFile) => {
+  return cy.task("parseXlsx", { filePath: inputFile });
+});
