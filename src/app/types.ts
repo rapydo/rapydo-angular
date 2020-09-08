ad62aaf56a67f933010c36c6d68d5a44613ea5ac
Mattia <m.dantonio81@gmail.com>
2020-09-08 08:14:57 +0200

    # Set readonly property to interfaces

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,132 +1,136 @@
 // It is used in Sessions response
 interface SimpleUser {
-  email: string;
-  name: string;
-  surname: string;
+  readonly email: string;
+  readonly name: string;
+  readonly surname: string;
 }
 
 // It is used in Groups response
 interface SimpleUserWithId extends SimpleUser {
-  uuid: string;
+  readonly uuid: string;
 }
 
 // It is used in Users response
 export interface Users extends Array<User> {}
 export interface User extends SimpleUserWithId {
-  isAdmin: boolean;
-  isLocalAdmin: boolean;
-  is_active: boolean;
-  privacy_accepted: boolean;
-  roles: any;
-  group?: Group;
+  readonly isAdmin: boolean;
+  readonly isLocalAdmin: boolean;
+  readonly is_active: boolean;
+  readonly privacy_accepted: boolean;
+  readonly roles: any;
+  readonly group?: Group;
 }
 
 export interface Sessions extends Array<Session> {}
 export interface Session {
-  token: string;
-  IP: string;
-  location: string;
-  user?: SimpleUser;
+  readonly token: string;
+  readonly IP: string;
+  readonly location: string;
+  readonly user?: SimpleUser;
 }
 
 export interface Groups extends Array<Group> {}
 export interface Group {
-  uuid: string;
-  shortname: string;
-  fullname: string;
-  coordinator?: SimpleUserWithId;
+  readonly uuid: string;
+  readonly shortname: string;
+  readonly fullname: string;
+  readonly coordinator?: SimpleUserWithId;
 }
 
 export interface Paging {
   page: number;
-  itemsPerPage: number;
+  readonly itemsPerPage: number;
   numPages: number;
   dataLength: number;
 }
 
 export interface Total {
-  total: number;
+  readonly total: number;
 }
 export interface Confirmation {
-  title: string;
-  message: string;
+  readonly title: string;
+  readonly message: string;
 }
 
 // key is optional only for back-compatibility
 // remove the ? once dropped all swagger compatibility rules
 export interface Schema {
-  type: string;
-  key: string;
-  label?: string;
-  description?: string;
-  default?: any;
-  format?: string;
-  required?: string;
+  readonly type: string;
+  readonly key: string;
+  readonly label?: string;
+  readonly description?: string;
+  readonly default?: any;
+  readonly format?: string;
+  readonly required?: string;
+  // this is initialized in formly service in case of type select
   options?: any[];
 
-  min?: number | Date;
-  max?: number | Date;
-  enum?: Record<string, string>;
+  readonly min?: number | Date;
+  readonly max?: number | Date;
+  readonly enum?: Record<string, string>;
 }
 
 export interface AdminStats {
-  boot_time: Date;
-  cpu: CPUStats;
-  disk: DiskStats;
-  io: IOStats;
-  network_latency: NetworkStats;
-  procs: ProcsStats;
-  ram: RAMStats;
-  swap: SwapStats;
+  readonly boot_time: Date;
+  readonly cpu: CPUStats;
+  readonly disk: DiskStats;
+  readonly io: IOStats;
+  readonly network_latency: NetworkStats;
+  readonly procs: ProcsStats;
+  readonly ram: RAMStats;
+  readonly swap: SwapStats;
 }
 
 interface CPUStats {
-  count: number;
-  load: number;
-  user: number;
-  idle: number;
-  wait: number;
-  system: number;
-  stolen: number;
+  /*
+   * @min 10
+   */
+  readonly count: number;
+  readonly load: number;
+  readonly user: number;
+  readonly idle: number;
+  readonly wait: number;
+  readonly system: number;
+  readonly stolen: number;
 }
 
 interface DiskStats {
-  free_disk_space: number;
-  total_disk_space: number;
-  used_disk_space: number;
-  occupacy: number;
+  readonly free_disk_space: number;
+  readonly total_disk_space: number;
+  readonly used_disk_space: number;
+  readonly occupacy: number;
 }
 
 interface IOStats {
-  blocks_received: number;
-  blocks_sent: number;
+  readonly blocks_received: number;
+  readonly blocks_sent: number;
 }
 
 interface NetworkStats {
-  min: number;
-  max: number;
-  avg: number;
+  readonly min: number;
+  readonly max: number;
+  readonly avg: number;
 }
 
 interface ProcsStats {
-  waiting_for_run: number;
-  uninterruptible_sleep: number;
+  readonly waiting_for_run: number;
+  readonly uninterruptible_sleep: number;
 }
 
 interface RAMStats {
-  active: number;
-  buffer: number;
-  cache: number;
-  free: number;
-  inactive: number;
-  total: number;
-  used: number;
+  readonly active: number;
+  readonly buffer: number;
+  readonly cache: number;
+  readonly free: number;
+  readonly inactive: number;
+  readonly total: number;
+  readonly used: number;
 }
 
 interface SwapStats {
-  free: number;
-  from_disk: number;
-  to_disk: number;
-  total: number;
-  used: number;
+  readonly free: number;
+  readonly from_disk: number;
+  readonly to_disk: number;
+  readonly total: number;
+  readonly used: number;
 }
