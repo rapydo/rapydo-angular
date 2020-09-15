3f1115fd5dadc2e493ce26e0e3c22f361f7747b1
Mattia <m.dantonio81@gmail.com>
2020-09-15 14:22:08 +0200

    # Bug fix /3

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -230,17 +230,21 @@ export class ApiService {
   //   }).pipe(catchError(this.parseErrorBlob));
   /* istanbul ignore next */
   public parseErrorBlob(err: HttpErrorResponse): Observable<any> {
-    if (!(err.error instanceof Blob)) {
-      return throwError(err.error);
+    if (err.error instanceof Blob) {
+      const obs = Observable.create((observer: any) => {
+        reader.onloadend = (e) => {
+          observer.error(JSON.parse(reader.result as string));
+          observer.complete();
+        };
+      });
+      reader.readAsText(err.error);
+      return obs;
     }
 
-    const obs = Observable.create((observer: any) => {
-      reader.onloadend = (e) => {
-        observer.error(JSON.parse(reader.result as string));
-        observer.complete();
-      };
-    });
-    reader.readAsText(err.error);
-    return obs;
+    if (err.error instanceof ProgressEvent) {
+      return throwError(err.message);
+    }
+
+    return throwError(err.error);
   }
 }
