427d9881713ccf33a9908da06775a182616c867d
Mattia <m.dantonio81@gmail.com>
2020-12-08 09:17:19 +0100

    # Split login.mocked tests into dedicated specs

--- cypress/integration/login.mocked.spec.ts
+++ cypress/integration/login.mocked.spec.ts
@@ -4,39 +4,13 @@
 import { getpassword } from "../../fixtures/utilities";
 
 describe("Mocked logins", () => {
-  beforeEach(() => {
+  it("Login - Missing or empty actions", () => {
     cy.visit("/app/login");
     cy.closecookielaw();
 
     cy.get("input[placeholder='Your username (email)']").as("user");
     cy.get("input[placeholder='Your password']").as("pwd");
-  });
 
-  // it("Login - Account not active", () => {
-
-  //   cy.intercept("POST", "/auth/login", {
-  //     statusCode: 403,
-  //     body: "Sorry, this account is not active",
-  //   }).as("post");
-
-  //   cy.get("@user").type(Cypress.env("AUTH_DEFAULT_USERNAME"));
-  //   cy.get("@pwd").type(Cypress.env("AUTH_DEFAULT_PASSWORD"));
-  //   cy.get("button").contains("Login").click();
-
-  //   cy.wait("@post");
-
-  //   cy.location().should((location) => {
-  //     expect(location.pathname).to.eq("/app/login");
-  //   });
-
-  //   cy.get("div.card-header.bg-warning h4").contains(
-  //     "This account is not active"
-  //   );
-  //   cy.get("div.card-block").contains("Didn't receive an activation link?");
-
-  // });
-
-  it("Login - Missing or empty actions", () => {
     // Cypress is still not able to override intercept..
     // Cannot set two or more intercepts on the same endpoint :-(
     cy.server();
@@ -99,122 +73,13 @@ describe("Mocked logins", () => {
     cy.server({ enable: false });
   });
 
-  it("Login - PASSWORD EXPIRED", () => {
-    // Cypress is still not able to override intercept..
-    // Cannot set two or more intercepts on the same endpoint :-(
-    cy.server();
-    cy.route({
-      method: "POST",
-      url: "/auth/login",
-      status: 403,
-      response: {
-        actions: ["PASSWORD EXPIRED"],
-        errors: ["Your password is expired, please change it"],
-      },
-    });
-
-    // Password changed by the previous test (FIRST LOGIN)
-    // This test (PASSWORD EXPIRED) will restore the default password at the end
-    const current_pwd = Cypress.env("AUTH_DEFAULT_PASSWORD") + "!";
-    const new_pwd = Cypress.env("AUTH_DEFAULT_PASSWORD");
-
-    cy.get("@user").type(Cypress.env("AUTH_DEFAULT_USERNAME"));
-    cy.get("@pwd").type(current_pwd);
-    cy.get("button").contains("Login").click();
-
-    cy.location().should((location) => {
-      expect(location.pathname).to.eq("/app/login");
-    });
-
-    cy.server({ enable: false });
-
-    cy.checkalert("Your password is expired, please change it");
-    cy.get("div.card-header h4").contains(
-      "Your password is expired, please change it"
-    );
-    cy.get("button").contains("Change").click();
-
-    cy.checkvalidation(0, "This field is required");
-    cy.checkvalidation(1, "This field is required");
-
-    cy.get("input[placeholder='Your new password']").as("new_pwd");
-    cy.get("input[placeholder='Confirm your new password']").as("pwd_confirm");
-
-    cy.get("@new_pwd").clear().type("short");
-    cy.checkvalidation(
-      0,
-      "Should have at least " +
-        Cypress.env("AUTH_MIN_PASSWORD_LENGTH") +
-        " characters"
-    );
-    cy.get("button").contains("Change").click();
-
-    cy.get("@new_pwd").clear().type(current_pwd);
-    cy.get("@pwd_confirm").clear().type("invalid");
-
-    cy.checkvalidation(0, "The password does not match");
-    cy.get("button").contains("Change").click();
-
-    cy.get("@pwd_confirm").clear().type(current_pwd);
-    cy.get("button").contains("Change").click();
-    cy.checkalert("The new password cannot match the previous password");
-
-    let pwd = getpassword(1);
-    cy.get("@new_pwd").clear().type(pwd);
-    cy.get("@pwd_confirm").clear().type(pwd);
-    cy.get("button").contains("Change").click();
-    cy.checkalert("Password is too weak, missing upper case letters");
-
-    cy.get("@new_pwd").clear().type(pwd.toUpperCase());
-    cy.get("@pwd_confirm").clear().type(pwd.toUpperCase());
-    cy.get("button").contains("Change").click();
-    cy.checkalert("Password is too weak, missing lower case letters");
-
-    pwd = getpassword(2);
-    cy.get("@new_pwd").clear().type(pwd);
-    cy.get("@pwd_confirm").clear().type(pwd);
-    cy.get("button").contains("Change").click();
-    cy.checkalert("Password is too weak, missing numbers");
-
-    pwd = getpassword(3);
-    cy.get("@new_pwd").clear().type(pwd);
-    cy.get("@pwd_confirm").clear().type(pwd);
-    cy.get("button").contains("Change").click();
-    cy.checkalert("Password is too weak, missing special characters");
-
-    // This will restore the default password
-    cy.get("@new_pwd").clear().type(new_pwd);
-    cy.get("@pwd_confirm").clear().type(new_pwd);
-    cy.get("button").contains("Change").click();
-  });
-
-  it("Login - TOTP", () => {
-    cy.intercept("POST", "/auth/login", {
-      statusCode: 403,
-      body: {
-        actions: ["TOTP"],
-        errors: ["You do not provided a valid second factor"],
-      },
-    }).as("post");
-
-    cy.get("@user").type(Cypress.env("AUTH_DEFAULT_USERNAME"));
-    cy.get("@pwd").type(Cypress.env("AUTH_DEFAULT_PASSWORD"));
-    cy.get("button").contains("Login").click();
-
-    cy.wait("@post");
-
-    cy.location().should((location) => {
-      expect(location.pathname).to.eq("/app/login");
-    });
-
-    cy.checkalert("You do not provided a valid second factor");
-    cy.get("div.card-header h4").contains("Provide the verification cod");
-    cy.get("button").contains("Authorize").click();
-
-    // fill the form! Not yet implemented
-  });
-
   it("Login - Unknown action", () => {
+    cy.visit("/app/login");
+    cy.closecookielaw();
+
+    cy.get("input[placeholder='Your username (email)']").as("user");
+    cy.get("input[placeholder='Your password']").as("pwd");
+
     cy.intercept("POST", "/auth/login", {
       statusCode: 403,
       body: {
