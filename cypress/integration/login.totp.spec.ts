ff192b8150925e945e7ae1020116364737a40700
Mattia <m.dantonio81@gmail.com>
2021-02-03 13:52:48 +0100

    # Enabled TOTP in integration full tests

--- cypress/integration/login.totp.spec.ts
+++ cypress/integration/login.totp.spec.ts
@@ -6,35 +6,33 @@ import { getpassword } from "../../fixtures/utilities";
 describe("Login", () => {
   if (Cypress.env("AUTH_SECOND_FACTOR_AUTHENTICATION")) {
     it("TOTP", () => {
+      const email = "aaaaaaaaaa000555" + Math.random() + "@sample.org";
+      const pwd = getpassword(4);
+
+      cy.login();
+      cy.createuser(email, pwd, true);
+      cy.logout();
+
       cy.visit("/app/login");
       cy.closecookielaw();
 
-      cy.get("input[placeholder='Your username (email)']").as("user");
-      cy.get("input[placeholder='Your password']").as("pwd");
-
-      cy.intercept("POST", "/auth/login", {
-        statusCode: 403,
-        body: {
-          actions: ["TOTP"],
-          errors: ["You do not provided a valid second factor"],
-        },
-      }).as("post");
-
-      cy.get("@user").type(Cypress.env("AUTH_DEFAULT_USERNAME"));
-      cy.get("@pwd").type(Cypress.env("AUTH_DEFAULT_PASSWORD"));
+      cy.get("input[placeholder='Your username (email)']").type(email);
+      cy.get("input[placeholder='Your password']").type(pwd);
       cy.get("button").contains("Login").click();
-
-      cy.wait("@post");
 
       cy.location().should((location) => {
         expect(location.pathname).to.eq("/app/login");
       });
 
       cy.checkalert("You do not provided a valid second factor");
-      cy.get("div.card-header h4").contains("Provide the verification cod");
+      cy.get("div.card-header h4").contains("Provide the verification code");
       cy.get("button").contains("Authorize").click();
 
       // fill the form! Not yet implemented
+
+      // cy.logout();
+      cy.login();
+      cy.deleteuser(email);
     });
   }
 });
