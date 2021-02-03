b4764ea9ab3b7d1246d3900de1c7331d5bdf7c20
Mattia <m.dantonio81@gmail.com>
2021-02-03 13:25:25 +0100

    # TOTP tests only executed if 2fa is enabled

--- cypress/integration/login.totp.spec.ts
+++ cypress/integration/login.totp.spec.ts
@@ -4,35 +4,37 @@
 import { getpassword } from "../../fixtures/utilities";
 
 describe("Login", () => {
-  it("TOTP", () => {
-    cy.visit("/app/login");
-    cy.closecookielaw();
+  if (Cypress.env("AUTH_SECOND_FACTOR_AUTHENTICATION")) {
+    it("TOTP", () => {
+      cy.visit("/app/login");
+      cy.closecookielaw();
 
-    cy.get("input[placeholder='Your username (email)']").as("user");
-    cy.get("input[placeholder='Your password']").as("pwd");
+      cy.get("input[placeholder='Your username (email)']").as("user");
+      cy.get("input[placeholder='Your password']").as("pwd");
 
-    cy.intercept("POST", "/auth/login", {
-      statusCode: 403,
-      body: {
-        actions: ["TOTP"],
-        errors: ["You do not provided a valid second factor"],
-      },
-    }).as("post");
+      cy.intercept("POST", "/auth/login", {
+        statusCode: 403,
+        body: {
+          actions: ["TOTP"],
+          errors: ["You do not provided a valid second factor"],
+        },
+      }).as("post");
 
-    cy.get("@user").type(Cypress.env("AUTH_DEFAULT_USERNAME"));
-    cy.get("@pwd").type(Cypress.env("AUTH_DEFAULT_PASSWORD"));
-    cy.get("button").contains("Login").click();
+      cy.get("@user").type(Cypress.env("AUTH_DEFAULT_USERNAME"));
+      cy.get("@pwd").type(Cypress.env("AUTH_DEFAULT_PASSWORD"));
+      cy.get("button").contains("Login").click();
 
-    cy.wait("@post");
+      cy.wait("@post");
 
-    cy.location().should((location) => {
-      expect(location.pathname).to.eq("/app/login");
+      cy.location().should((location) => {
+        expect(location.pathname).to.eq("/app/login");
+      });
+
+      cy.checkalert("You do not provided a valid second factor");
+      cy.get("div.card-header h4").contains("Provide the verification cod");
+      cy.get("button").contains("Authorize").click();
+
+      // fill the form! Not yet implemented
     });
-
-    cy.checkalert("You do not provided a valid second factor");
-    cy.get("div.card-header h4").contains("Provide the verification cod");
-    cy.get("button").contains("Authorize").click();
-
-    // fill the form! Not yet implemented
-  });
+  }
 });
