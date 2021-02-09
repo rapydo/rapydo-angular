ca5b44fc0a363526845a6123a6fece3334af3f0d
Mattia <m.dantonio81@gmail.com>
2021-02-09 11:24:20 +0100

    # Debug code /2

--- cypress/integration/login.ban.spec.ts
+++ cypress/integration/login.ban.spec.ts
@@ -4,6 +4,20 @@
 import { getpassword, get_totp } from "../../fixtures/utilities";
 
 describe("Login Ban", () => {
+  it("Debug code", () => {
+    cy.visit("/app/login");
+    cy.get("input[placeholder='Your username (email)']").type(
+      Cypress.env("AUTH_MAX_LOGIN_ATTEMPTS")
+    );
+
+    let max = 0 + Cypress.env("AUTH_MAX_LOGIN_ATTEMPTS");
+    if (max < 10) {
+      max = 10;
+    }
+
+    cy.get("input[placeholder='Your username (email)']").type(" -> ");
+    cy.get("input[placeholder='Your username (email)']").type(max);
+  });
   if (Cypress.env("AUTH_MAX_LOGIN_ATTEMPTS") > 0) {
     it("Ban after wrong password", () => {
       for (let i = 0; i < Cypress.env("AUTH_MAX_LOGIN_ATTEMPTS"); i++) {
