a90eef602afbfcaac7ef0ead4daf7c6e6337b3bc
Mattia <m.dantonio81@gmail.com>
2021-02-09 12:19:40 +0100

    Added integration test on login ban

--- cypress/integration/login.ban.spec.ts
+++ cypress/integration/login.ban.spec.ts
@@ -4,33 +4,75 @@
 import { getpassword, get_totp } from "../../fixtures/utilities";
 
 describe("Login Ban", () => {
-  it("Debug code", () => {
-    cy.visit("/app/login");
-    cy.get("input[placeholder='Your username (email)']").type(
-      Cypress.env("AUTH_MAX_LOGIN_ATTEMPTS")
-    );
-
-    let max = 0 + Cypress.env("AUTH_MAX_LOGIN_ATTEMPTS");
-    if (max < 10) {
-      max = 10;
-    }
-
-    cy.get("input[placeholder='Your username (email)']").type(" -> ");
-    cy.get("input[placeholder='Your username (email)']").type(max);
-  });
   if (Cypress.env("AUTH_MAX_LOGIN_ATTEMPTS") > 0) {
     it("Ban after wrong password", () => {
-      for (let i = 0; i < Cypress.env("AUTH_MAX_LOGIN_ATTEMPTS"); i++) {
-        cy.visit("/app/login");
+      const email = "bbbb111111111" + Math.random() + "@sample.org";
+      const pwd = getpassword(4);
+
+      cy.createuser(email, pwd);
+
+      // 10 is the default used by backend (services/authentication) in testing mode
+      const max_failures = Math.max(Cypress.env("AUTH_MAX_LOGIN_ATTEMPTS"), 10);
+
+      cy.visit("/app/login");
+      cy.get("div.card-header h4").contains("Login");
+      cy.get("input[placeholder='Your username (email)']").clear().type(email);
+      for (let i = 0; i < max_failures; i++) {
+        cy.get("input[placeholder='Your password']")
+          .clear()
+          .type(getpassword(4));
+        cy.get("button").contains("Login").click();
+        cy.checkalert("Invalid access credentials");
       }
 
-      for (let i = 0; i < 2; i++) {
-        cy.visit("/app/bah");
-      }
+      cy.get("input[placeholder='Your password']").clear().type(pwd);
+      cy.get("button").contains("Login").click();
+      cy.checkalert(
+        "Sorry, this account is temporarily blocked due to the number of failed login attempts."
+      );
+
+      cy.login();
+      cy.deleteuser(email);
     });
 
     if (Cypress.env("AUTH_SECOND_FACTOR_AUTHENTICATION")) {
-      it("Ban after wrong totp", () => {});
+      it("Ban after wrong totp", () => {
+        const email = "bbbb222222222" + Math.random() + "@sample.org";
+        const pwd = getpassword(4);
+
+        cy.createuser(email, pwd);
+        // 10 is the default used by backend (services/authentication) in testing mode
+        const max_failures = Math.max(
+          Cypress.env("AUTH_MAX_LOGIN_ATTEMPTS"),
+          10
+        );
+
+        cy.visit("/app/login");
+        cy.get("div.card-header h4").contains("Login");
+        cy.get("input[placeholder='Your username (email)']")
+          .clear()
+          .type(email);
+        cy.get("input[placeholder='Your password']").clear().type(pwd);
+        cy.get("button").contains("Login").click();
+
+        cy.get("input[placeholder='Your password']").should("not.exist");
+        cy.get("div.card-header h4").contains("Provide the verification code");
+
+        for (let i = 0; i < max_failures; i++) {
+          cy.get("input[placeholder='TOTP verification code']").type("000000");
+          cy.get("button").contains("Authorize").click();
+          cy.checkalert("Verification code is not valid");
+        }
+
+        cy.get("input[placeholder='TOTP verification code']").type(get_totp());
+        cy.get("button").contains("Authorize").click();
+        cy.checkalert(
+          "Sorry, this account is temporarily blocked due to the number of failed login attempts."
+        );
+
+        cy.login();
+        cy.deleteuser(email);
+      });
     }
   }
 });
