3737bdd7253b2827433b430d7a15cce79533c254
Mattia <m.dantonio81@gmail.com>
2021-02-09 10:42:38 +0100

    # Preliminary check to test login ban on cypress

---
+++ cypress/integration/login.ban.spec.ts
@@ -0,0 +1,18 @@
+// This is to silence ESLint about undefined cy
+/*global cy, Cypress*/
+
+import { getpassword, get_totp } from "../../fixtures/utilities";
+
+describe("Login Ban", () => {
+  if (Cypress.env("AUTH_MAX_LOGIN_ATTEMPTS") != "0") {
+    it("Ban after wrong password", () => {
+      for (let i = 0; i < Cypress.env("AUTH_MAX_LOGIN_ATTEMPTS"); i++) {
+        cy.visit("/app/login");
+      }
+    });
+
+    if (Cypress.env("AUTH_SECOND_FACTOR_AUTHENTICATION")) {
+      it("Ban after wrong totp", () => {});
+    }
+  }
+});
