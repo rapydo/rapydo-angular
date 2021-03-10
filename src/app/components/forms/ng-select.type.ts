28031ad86b2bd84f482c9f00607a7239e9f86428
Mattia <m.dantonio81@gmail.com>
2021-03-10 16:05:39 +0100

    Added debounceTime to ng-select autocomplete callback

--- src/app/components/forms/ng-select.type.ts
+++ src/app/components/forms/ng-select.type.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from "@angular/core";
 import { FieldType } from "@ngx-formly/core";
 import { concat, Observable, of, Subject } from "rxjs";
-import { catchError, distinctUntilChanged, switchMap, tap, filter, delay } from 'rxjs/operators';
+import {
+  catchError,
+  distinctUntilChanged,
+  switchMap,
+  tap,
+  filter,
+  delay,
+  debounceTime,
+} from "rxjs/operators";
 import { ApiService } from "@rapydo/services/api";
 
 interface Item {
   [key: string]: string;
 }
 
-
 @Component({
-  selector: 'formly-ng-select',
-  template:`
+  selector: "formly-ng-select",
+  template: `
     <div>
       <ng-select
         [items]="items$ | async"
@@ -25,17 +32,18 @@ interface Item {
         [trackByFn]="trackByFn"
       >
         <ng-template ng-option-tmp let-item="item" let-search="searchTerm">
-            <div class="ng-option-label"><span [ngOptionHighlight]="search">{{item[bindLabel]}}</span></div>
-            <ng-container *ngIf="showValue">
-              <small><b>ID:</b> {{item[bindValue]}}</small>
-            </ng-container>
+          <div class="ng-option-label">
+            <span [ngOptionHighlight]="search">{{ item[bindLabel] }}</span>
+          </div>
+          <ng-container *ngIf="showValue">
+            <small><b>ID:</b> {{ item[bindValue] }}</small>
+          </ng-container>
         </ng-template>
       </ng-select>
     </div>
-  `
+  `,
 })
 export class NgSelectFormlyComponent extends FieldType implements OnInit {
-
   itemLoading: boolean = false;
   showValue: boolean = false;
   items$: Observable<Item[]>;
@@ -44,15 +52,15 @@ export class NgSelectFormlyComponent extends FieldType implements OnInit {
   bindValue: string;
   bindLabel: string;
 
-  constructor(private api: ApiService){
+  constructor(private api: ApiService) {
     super();
   }
 
   ngOnInit() {
-    this.endpoint = this.to['endpoint'];
+    this.endpoint = this.to["endpoint"];
     this.showValue = this.to.showValue || false;
-    this.bindValue = this.to.bindValue || 'value';
-    this.bindLabel = this.to.bindLabel || 'label';
+    this.bindValue = this.to.bindValue || "value";
+    this.bindLabel = this.to.bindLabel || "label";
 
     this.loadItems();
   }
@@ -61,20 +69,22 @@ export class NgSelectFormlyComponent extends FieldType implements OnInit {
     return item[this.bindValue];
   }
 
-
   private loadItems() {
     this.items$ = concat(
-      of(this.to["selectedItems"]),  // default items
-      of([]).pipe(delay(500)),  // clean-up options
+      of(this.to["selectedItems"]), // default items
+      of([]).pipe(delay(500)), // clean-up options
       this.itemInput$.pipe(
-        filter(v => v !== ''),
+        debounceTime(300),
+        filter((v) => v !== null && v !== ""),
         distinctUntilChanged(),
-        tap(() => this.itemLoading = true),
-        switchMap(term => this.api.get<Item[]>(`${this.endpoint}/${term}`).pipe(
+        tap(() => (this.itemLoading = true)),
+        switchMap((term) =>
+          this.api.get<Item[]>(`${this.endpoint}/${term}`).pipe(
             catchError(() => of([])), // empty list on error
-            tap(() => this.itemLoading = false)
-        )))
-    )
+            tap(() => (this.itemLoading = false))
+          )
+        )
+      )
+    );
   }
-
 }
