05f8e1213ba0f2d0b4f3cfe0d1e741c3111e0b49
Mattia <m.dantonio81@gmail.com>
2021-06-25 05:11:32 +0200

    # Increased keystrokeDelay from 100 to 150 msec

--- cypress/integration/admin_sessions.spec.ts
+++ cypress/integration/admin_sessions.spec.ts
@@ -14,7 +14,7 @@ describe("AdminSessions", () => {
 
   // This is the same as in profile.sessions.spec
   // Please note the keystrokeDelay needed because there is no debounceTime on search
-  it("Sort, search, copy", { keystrokeDelay: 100 }, () => {
+  it("Sort, search, copy", { keystrokeDelay: 150 }, () => {
     cy.scrollTo("bottom");
     cy.get("div.page-count").contains(" total");
     cy.get("ul.pager").find("li.pages:contains(' 1 ')");
