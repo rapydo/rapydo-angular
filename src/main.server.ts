8f4f16850e5359c58c6918326bb4d71748dea38a
Mattia <m.dantonio81@gmail.com>
2021-07-07 11:39:53 +0200

    Undo introduction of domino and testing mock-browser

--- src/main.server.ts
+++ src/main.server.ts
@@ -2,19 +2,10 @@ import { enableProdMode } from "@angular/core";
 import { environment } from "@rapydo/../environments/environment";
 
 import "localstorage-polyfill";
-import * as domino from "domino";
-import { join } from "path";
 
-// import * as fs from "fs";
-
-// Use the browser index.html as template for the mock window
-// const template = fs.readFileSync("///index.html").toString();
-const template = join("/app", "dist", "index.html");
-
-// Shim for the global window and document objects.
-const window: any = domino.createWindow(template);
-global["window"] = window;
-global["document"] = window.document;
+const MockBrowser = require("mock-browser").mocks.MockBrowser;
+const mock = new MockBrowser();
+global["window"] = mock.getWindow();
 global["localStorage"] = localStorage;
 
 if (environment.production) {
