606c28d4a6922d31056cd67110ef18297f42b72f
Mattia <m.dantonio81@gmail.com>
2021-07-07 11:16:22 +0200

    # Fix introduced domino to mock window and document during SSR

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,8 +1,6 @@
 import { enableProdMode } from "@angular/core";
 import { environment } from "@rapydo/../environments/environment";
 
-import "zone.js/dist/zone";
-
 import "localstorage-polyfill";
 import * as domino from "domino";
 import { join } from "path";
@@ -11,7 +9,7 @@ import { join } from "path";
 
 // Use the browser index.html as template for the mock window
 // const template = fs.readFileSync("///index.html").toString();
-const template = join("app", "dist", "index.html");
+const template = join("/app", "dist", "index.html");
 
 // Shim for the global window and document objects.
 const window: any = domino.createWindow(template);
@@ -23,5 +21,6 @@ if (environment.production) {
   enableProdMode();
 }
 
+import "zone.js/dist/zone";
 export { AppServerModule } from "@rapydo/app.server.module";
 export { renderModule, renderModuleFactory } from "@angular/platform-server";
