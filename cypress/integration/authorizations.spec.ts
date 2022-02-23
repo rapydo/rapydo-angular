b9322152c92b894f0ce8631aa6f75011e14fac10
Mattia D'Antonio <m.dantonio81@gmail.com>
2022-02-23 15:49:53 +0100

    # Disabled authorizations test on staff role when not enabled

--- cypress/integration/authorizations.spec.ts
+++ cypress/integration/authorizations.spec.ts
@@ -76,86 +76,88 @@ describe("Test Authorizations", () => {
     cy.deleteuser(email);
   });
 
-  it("Test Staff authorizations", () => {
-    const email = get_random_username("staff");
-    const pwd = getpassword(4);
-    // ....................., expired, init, roles
-    cy.createuser(email, pwd, false, true, ["staff", "user"]);
-    cy.login(email, pwd);
+  if (Cypress.env("AUTH_ROLES").includes(",staff_user,")) {
+    it("Test Staff authorizations", () => {
+      const email = get_random_username("staff");
+      const pwd = getpassword(4);
+      // ....................., expired, init, roles
+      cy.createuser(email, pwd, false, true, ["staff", "user"]);
+      cy.login(email, pwd);
 
-    ////////////////////////////////////////////////////////////////////
-    cy.goto_profile();
-    cy.location().should((location) => {
-      expect(location.pathname).to.eq("/app/profile");
-    });
-    cy.get("div.card-header h1");
-    ////////////////////////////////////////////////////////////////////
-    cy.visit("/app/profile/changepassword");
-    cy.location().should((location) => {
-      expect(location.pathname).to.eq("/app/profile/changepassword");
-    });
-    cy.get("div.card-header h1");
-    ////////////////////////////////////////////////////////////////////
-    cy.visit("/app/profile/sessions");
-    cy.location().should((location) => {
-      expect(location.pathname).to.eq("/app/profile/sessions");
-    });
-    cy.get("div.card-header h1");
-    ////////////////////////////////////////////////////////////////////
-    cy.visit("/app/admin/users");
-    cy.location().should((location) => {
-      expect(location.pathname).to.eq("/app/admin/users");
-    });
-    cy.get("div.card-header h1");
-    ////////////////////////////////////////////////////////////////////
-    cy.visit("/app/admin/groups");
-    cy.location().should((location) => {
-      expect(location.pathname).to.eq("/app/admin/groups");
-    });
-    cy.get("div.card-header h1");
-    ////////////////////////////////////////////////////////////////////
-    cy.visit("/app/admin/sessions");
-    cy.location().should((location) => {
-      expect(location.pathname).to.not.eq("/app/admin/sessions");
-    });
-    cy.checkalert(
-      "Permission denied: you are not authorized to access this page"
-    );
-    cy.login(email, pwd);
-    ////////////////////////////////////////////////////////////////////
-    cy.visit("/app/admin/stats");
-    cy.location().should((location) => {
-      expect(location.pathname).to.not.eq("/app/admin/stats");
-    });
-    cy.checkalert(
-      "Permission denied: you are not authorized to access this page"
-    );
-    cy.login(email, pwd);
-    ////////////////////////////////////////////////////////////////////
-    cy.visit("/app/admin/mail");
-    cy.location().should((location) => {
-      expect(location.pathname).to.not.eq("/app/admin/mail");
-    });
-    cy.checkalert(
-      "Permission denied: you are not authorized to access this page"
-    );
-    cy.login(email, pwd);
-    ////////////////////////////////////////////////////////////////////
-    cy.visit("/app/group/users");
-    cy.location().should((location) => {
-      expect(location.pathname).to.not.eq("/app/group/users");
-    });
-    cy.checkalert(
-      "Permission denied: you are not authorized to access this page"
-    );
-    // cy.login(email, pwd);
-    ////////////////////////////////////////////////////////////////////
+      ////////////////////////////////////////////////////////////////////
+      cy.goto_profile();
+      cy.location().should((location) => {
+        expect(location.pathname).to.eq("/app/profile");
+      });
+      cy.get("div.card-header h1");
+      ////////////////////////////////////////////////////////////////////
+      cy.visit("/app/profile/changepassword");
+      cy.location().should((location) => {
+        expect(location.pathname).to.eq("/app/profile/changepassword");
+      });
+      cy.get("div.card-header h1");
+      ////////////////////////////////////////////////////////////////////
+      cy.visit("/app/profile/sessions");
+      cy.location().should((location) => {
+        expect(location.pathname).to.eq("/app/profile/sessions");
+      });
+      cy.get("div.card-header h1");
+      ////////////////////////////////////////////////////////////////////
+      cy.visit("/app/admin/users");
+      cy.location().should((location) => {
+        expect(location.pathname).to.eq("/app/admin/users");
+      });
+      cy.get("div.card-header h1");
+      ////////////////////////////////////////////////////////////////////
+      cy.visit("/app/admin/groups");
+      cy.location().should((location) => {
+        expect(location.pathname).to.eq("/app/admin/groups");
+      });
+      cy.get("div.card-header h1");
+      ////////////////////////////////////////////////////////////////////
+      cy.visit("/app/admin/sessions");
+      cy.location().should((location) => {
+        expect(location.pathname).to.not.eq("/app/admin/sessions");
+      });
+      cy.checkalert(
+        "Permission denied: you are not authorized to access this page"
+      );
+      cy.login(email, pwd);
+      ////////////////////////////////////////////////////////////////////
+      cy.visit("/app/admin/stats");
+      cy.location().should((location) => {
+        expect(location.pathname).to.not.eq("/app/admin/stats");
+      });
+      cy.checkalert(
+        "Permission denied: you are not authorized to access this page"
+      );
+      cy.login(email, pwd);
+      ////////////////////////////////////////////////////////////////////
+      cy.visit("/app/admin/mail");
+      cy.location().should((location) => {
+        expect(location.pathname).to.not.eq("/app/admin/mail");
+      });
+      cy.checkalert(
+        "Permission denied: you are not authorized to access this page"
+      );
+      cy.login(email, pwd);
+      ////////////////////////////////////////////////////////////////////
+      cy.visit("/app/group/users");
+      cy.location().should((location) => {
+        expect(location.pathname).to.not.eq("/app/group/users");
+      });
+      cy.checkalert(
+        "Permission denied: you are not authorized to access this page"
+      );
+      // cy.login(email, pwd);
+      ////////////////////////////////////////////////////////////////////
 
-    // Delete temporary user
-    // cy.logout();
-    cy.login();
-    cy.deleteuser(email);
-  });
+      // Delete temporary user
+      // cy.logout();
+      cy.login();
+      cy.deleteuser(email);
+    });
+  }
 
   it("Test Coordinator authorizations", () => {
     const email = get_random_username("coordinator");
