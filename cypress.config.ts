46ea94f69bd3df711b21ec25b74279631db6b816
Mattia D'Antonio <m.dantonio81@gmail.com>
2022-06-04 11:11:07 +0200

    # Disabled Cypress supportFile

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:8080",
     watchForFileChanges: false,
+    supportFile: false,
     screenshotsFolder: "/cypress",
     videosFolder: "/cypress",
     downloadsFolder: "/cypress",
