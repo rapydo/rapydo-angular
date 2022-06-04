166895b4c323316260b4cc85d943dcb3c8da341e
Mattia D'Antonio <m.dantonio81@gmail.com>
2022-06-04 14:26:10 +0200

    Replaced deprecated cypress plugin file with setupNodeEvents configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -13,4 +13,16 @@ export default defineConfig({
       runMode: 0,
     },
   },
+  component: {
+    setupNodeEvents(on, config) {
+      require("@cypress/code-coverage/task")(on, config);
+      require("cypress-fail-fast/plugin")(on, config);
+
+      // add other tasks to be registered here
+
+      // IMPORTANT to return the config object
+      // with the any changed environment variables
+      return config;
+    },
+  },
 });
