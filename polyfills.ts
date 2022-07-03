95560314258656d716b9fa5aa95d1a41308ae8c8
Mattia D'Antonio <m.dantonio81@gmail.com>
2022-07-03 08:42:57 +0200

    # run prettier on all files

--- polyfills.ts
+++ polyfills.ts
@@ -90,6 +90,6 @@ import "@angular/localize/init";
 
 /* To reference and create an instance of the Buffer. This requires to install the buffer package. */
 (window as any).global = window;
-declare var global: (any);
-declare var require: (any);
-global.Buffer = global.Buffer || require('buffer').Buffer;
+declare var global: any;
+declare var require: any;
+global.Buffer = global.Buffer || require("buffer").Buffer;
