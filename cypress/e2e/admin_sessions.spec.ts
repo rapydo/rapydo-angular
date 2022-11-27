98ff19ea6b10f41d52e7ed0b1bb86da6cb2314b3
Mattia D'Antonio <m.dantonio81@gmail.com>
2022-11-27 08:23:01 +0100

    # Added retry to flaky test

--- cypress/e2e/admin_sessions.spec.ts
+++ cypress/e2e/admin_sessions.spec.ts
@@ -13,84 +13,102 @@ describe("AdminSessions", () => {
   });
 
   // This is the same as in profile.sessions.spec
-  it("Sort, search, copy", () => {
-    cy.scrollTo("bottom");
-    cy.get("div.page-count").contains(" total");
-    cy.get("ul.pager").find("li.pages:contains(' 1 ')");
-    cy.get("ul.pager").find("li.pages:contains(' 2 ')").click();
-    cy.get("ul.pager").find("li.pages:contains(' 1 ')").click();
-    cy.scrollTo("top");
+  it(
+    "Sort, search, copy",
+    {
+      retries: {
+        runMode: 3,
+        openMode: 1,
+      },
+    },
+    () => {
+      cy.scrollTo("bottom");
+      cy.get("div.page-count").contains(" total");
+      cy.get("ul.pager").find("li.pages:contains(' 1 ')");
+      cy.get("ul.pager").find("li.pages:contains(' 2 ')").click();
+      cy.get("ul.pager").find("li.pages:contains(' 1 ')").click();
+      cy.scrollTo("top");
 
-    cy.get("span.datatable-header-cell-label").contains("Expiration").click();
-    cy.get("datatable-body-row").first().find(".fa-trash");
+      cy.get("span.datatable-header-cell-label").contains("Expiration").click();
+      cy.get("datatable-body-row").first().find(".fa-trash");
 
-    // this does not work because server side sort is not implemented:
-    // Already sorted by Expiration, current token is the last
-    // Sort by Expiration, current token is now the first
-    // cy.get("span.datatable-header-cell-label").contains("Expiration").click();
-    // cy.get("datatable-body-row").first().find(".fa-trash").should("not.exist");
+      // this does not work because server side sort is not implemented:
+      // Already sorted by Expiration, current token is the last
+      // Sort by Expiration, current token is now the first
+      // cy.get("span.datatable-header-cell-label").contains("Expiration").click();
+      // cy.get("datatable-body-row").first().find(".fa-trash").should("not.exist");
 
-    cy.get('input[placeholder="Type to filter sessions"]').as("filter");
+      cy.get('input[placeholder="Type to filter sessions"]').as("filter");
 
-    cy.get("datatable-body-row").its("length").should("be.gte", 1);
+      cy.get("datatable-body-row").its("length").should("be.gte", 1);
 
-    // Not probable to have five consective Ws
-    cy.get("@filter").clear().type("WWWWW");
-    // debounceTime is set to 200
-    cy.wait(250);
+      // Not probable to have five consective Ws
+      cy.get("@filter").clear().type("WWWWW");
+      // debounceTime is set to 200
+      cy.wait(250);
 
-    cy.get("datatable-body-row").should("have.length", 0);
+      cy.get("datatable-body-row").should("have.length", 0);
 
-    // Filter by username
-    cy.get("@filter").clear().type(Cypress.env("AUTH_DEFAULT_USERNAME"));
-    // debounceTime is set to 200
-    cy.wait(250);
+      // Filter by username
+      cy.get("@filter").clear().type(Cypress.env("AUTH_DEFAULT_USERNAME"));
+      // debounceTime is set to 200
+      cy.wait(250);
 
-    cy.get("datatable-body-row").its("length").should("be.gte", 1);
+      cy.get("datatable-body-row").its("length").should("be.gte", 1);
 
-    // Filter by location (only Unknown sessions should be included here)
-    cy.get("@filter").clear().type("Unknown");
-    // debounceTime is set to 200
-    cy.wait(250);
+      // Filter by location (only Unknown sessions should be included here)
+      cy.get("@filter").clear().type("Unknown");
+      // debounceTime is set to 200
+      cy.wait(250);
 
-    cy.get("datatable-body-row").its("length").should("be.gte", 1);
+      cy.get("datatable-body-row").its("length").should("be.gte", 1);
 
-    // Filter by IP
-    cy.get("datatable-body-row")
-      .first()
-      .find("datatable-body-cell")
-      .eq(1)
-      .then(($cell) => {
-        const IP = $cell.text();
+      // Filter by IP
+      cy.get("datatable-body-row")
+        .first()
+        .find("datatable-body-cell")
+        .eq(1)
+        .then(($cell) => {
+          const IP = $cell.text();
 
-        cy.get("@filter").clear().type(IP);
-        // debounceTime is set to 200
-        cy.wait(250);
+          cy.get("@filter").clear().type(IP);
+          // debounceTime is set to 200
+          cy.wait(250);
 
-        cy.get("datatable-body-row").its("length").should("be.gte", 1);
-      });
+          cy.get("datatable-body-row").its("length").should("be.gte", 1);
+        });
 
-    // This is the same as in profile.sessions.spec
-    cy.get("datatable-body-row").first().find(".fa-clipboard").click();
-    // cy.checkalert("Token successfully copied");
+      // This is the same as in profile.sessions.spec
+      cy.get("datatable-body-row").first().find(".fa-clipboard").click();
+      // cy.checkalert("Token successfully copied");
 
-    // Clipboard verification requires an additional plugin...
-  });
+      // Clipboard verification requires an additional plugin...
+    }
+  );
 
   // This is the same as in profile.sessions.spec
-  it("Delete", () => {
-    cy.get("span.datatable-header-cell-label").contains("Expiration").click();
-    // due to scroll not work we cannot visualize firt and last tokens...
-    // let's click in the middle...
-    cy.get("datatable-body-row").eq(15).find(".fa-trash").click();
-    cy.get("h2.modal-title").contains("Confirmation required");
-    cy.get("button").contains("No, cancel").click();
-    cy.get("datatable-body-row").eq(15).find(".fa-trash").click();
-    cy.get("h2.modal-title").contains("Confirmation required");
-    cy.get("button").contains("Yes, delete").click();
+  it(
+    "Delete",
+    {
+      retries: {
+        runMode: 3,
+        openMode: 1,
+      },
+    },
+    () => {
+      cy.get("span.datatable-header-cell-label").contains("Expiration").click();
+      // due to scroll not work we cannot visualize firt and last tokens...
+      // let's click in the middle...
+      cy.get("datatable-body-row").eq(15).find(".fa-trash").click();
+      cy.get("h2.modal-title").contains("Confirmation required");
+      cy.get("button").contains("No, cancel").click();
+      cy.get("datatable-body-row").eq(15).find(".fa-trash").click();
+      cy.get("h2.modal-title").contains("Confirmation required");
+      cy.get("button").contains("Yes, delete").click();
 
-    cy.checkalert("Confirmation: token successfully deleted");
-  });
+      cy.checkalert("Confirmation: token successfully deleted");
+    }
+  );
 
   it("Backend errors", () => {
     cy.intercept("GET", /\/api\/admin\/tokens\?*/, {
